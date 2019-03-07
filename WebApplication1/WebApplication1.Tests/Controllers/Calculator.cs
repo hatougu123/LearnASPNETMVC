1b5ffaca6b00971a71b4cb3ff2aecf0bc7b34a1a
NamTran <trankhanhphuongnam@gmail.com>
2019-03-07 17:48:26 +0700

    First Lesson

---
+++ WebApplication1/WebApplication1.Tests/Controllers/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Controllers;
+using System.Web.Mvc;
+
+namespace WebApplication1.Tests.Controllers
+{
+    [TestClass]
+    public class Calculator
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            // Arrange
+            var controller= new CalculatorController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void TestShowAuthor()
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        //
+        // GET: /Calculator/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string ShowAuthor()
+        {
+            return "Phuong Nam";
+        }
+        public double Factorial(int id)
+        {
+            int n = id;
+            double f = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                f *= i; ;
+            }
+            return f;
+        }
+        public int Sum(int a, int b)
+        {
+            int s;
+            s = a + b;
+            return s;
+        }
+	}
+}
