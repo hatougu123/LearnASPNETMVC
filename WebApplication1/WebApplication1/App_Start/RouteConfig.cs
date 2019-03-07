6d4fe3c474996567a5ec7e6128bffe4de8cb289e
NamTran <trankhanhphuongnam@gmail.com>
2019-03-07 16:54:43 +0700

    add new welcome

--- WebApplication1/WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/WebApplication1/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace WebApplication1
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Welcome",
+                url:"Welcome/",
+                defaults: new {controller ="Home", action = "Index"}
+            );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
